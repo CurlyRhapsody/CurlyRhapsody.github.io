b815a3de598f98e10c6d4920e6b918bf1584278e
CurlyRhapsody <matthewchan60792@gmail.com>
2021-06-05 19:57:42 +0800

    Hope this is the last fix

---
+++ js/ForumSim.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var floor = 1;
+
+function PostComment() {
+    ReactDOM.render(<NewComment />, document.getElementById('CSection'));
+}
+
+class PostButton extends React.Component {
+    render() {
+        return <button onclick={PostComment()}>Submit</button>
+    }
+}
+
+ReactDOM.render(<PostButton />, document.getElementById('submit'));
+
+class NewComment extends React.Component {
+    constructor() {
+        super();
+        floor += 1;
+        this.name = document.getElementById('name').value;
+        this.content = document.getElementById('content').value;
+        this.date = new Date();
+    }
+
+    render() {
+        return (
+            <div style="margin='2px solid black'">
+                <p style="text-align: left; line-height: 0px;">#{floor} <b>{this.name}</b> @ {this.date}</p>
+                <p style="text-align: left; line-height: 0px;">{this.content}</p>
+            </div>
+        );
+    }
+}
