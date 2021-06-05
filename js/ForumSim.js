205b1edc8ccd987b7312ece811b70141d552b655
CurlyRhapsody <matthewchan60792@gmail.com>
2021-06-05 17:42:42 +0800

    Add new project Prototype

---
+++ js/ForumSim.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var floor = 1;
+
+class NewComment extends React.Component {
+    constructor() {
+        super();
+        floor += 1;
+        this.name = document.getElementById('name').value;
+        this.content = document.getElementById('content').value;
+        this.date = new Date();
+    }
+
+    render() {
+        return (
+            <div style="margin='2px solid black'">
+                <p style="text-align: left; line-height: 0px;">#{floor} <b>{this.name}</b> @ {this.date}</p>
+                <p style="text-align: left; line-height: 0px;">{this.content}</p>
+            </div>
+        );
+    }
+}
+
+function PostComment() {
+    ReactDOM.render(<NewComment />, document.getElementById('root'));
+}
