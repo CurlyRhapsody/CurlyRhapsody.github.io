93c17becea453efca43cdc6f6e7e8dd61ed6f2c4
CurlyRhapsody <matthewchan60792@gmail.com>
2026-04-24 23:54:01 +0800

    Filled up some home page stuff blah blah

---
+++ src/app/components/CustomComponents.tsx
@@ -0,0 +1,5 @@
+import { Stack, styled } from "@mui/material";
+
+export const ShadowedStack = styled(Stack)({
+    boxShadow: "0.375rem 0.375rem 0.375rem -0.125px rgba(0,0,0,0.5);"
+});
--- src/app/components/pages/HomePage.tsx
+++ src/app/components/pages/HomePage.tsx
@@ -4,11 +4,16 @@ import { Box, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import HomePageIamge from "../../assets/images/homePage.jpg";
 import { motion } from "motion/react";
-import { Title2 } from "../Styled/Text";
+import { useTranslations } from "next-intl";
+import { Body1, Subtitle1, Subtitle2, Title1, Title2 } from "../Styled/Text";
+import { ShadowedStack } from "../CustomComponents";
 
 const HomePage = () => {
+
+    const t = useTranslations("home");
+
     return (
-        <Box sx={{ width: "100%" }}>
+        <Stack sx={{ width: "100%", gap: "2rem", mb: "5rem" }}>
             <Box
                 sx={{ position: "relative", width: "100%", overflow: "hidden", aspectRatio: "16/9", objectPosition: "center" }}
             >
@@ -24,26 +29,62 @@ const HomePage = () => {
                 >
                     <Stack
                         sx={{
-                            position: "absolute", left: "calc(50% - min(25%, 20rem))", top: "calc(50% - min(25%, 11.25rem))",
-                            bgcolor: "#FFFFFFCC", width: "50%", maxWidth: "40rem", height: "50%", maxHeight: "22.5rem", borderRadius: "2rem",
-                            alignItems: "center", padding: "2rem"
+                            position: "absolute", left: "calc(50% - 20rem)", top: "calc(50% - 11.25rem)",
+                            bgcolor: "#FFFFFFCC", width: "40rem", height: "22.5rem", borderRadius: "2rem",
+                            alignItems: "center", padding: "2rem", gap: "3rem"
                         }}
                     >
-                        <Typography 
-                            sx={{
-                                fontSize: "4rem", lineHeight: "5.6rem", fontWeight: 800,
-                                background: "-webkit-linear-gradient(15deg, #1E90FF, #7B68EE)",
-                                WebkitBackgroundClip: "text",
-                                WebkitTextFillColor: "transparent",
-                            }}
-                        >
-                            Imagination
-                        </Typography>
+                        <Box sx={{ textAlign: "center" }}>
+                            <Typography 
+                                sx={{
+                                    fontSize: "5rem", lineHeight: "7rem", fontWeight: 800,
+                                    background: "-webkit-linear-gradient(15deg, #1E90FF, #7B68EE)",
+                                    WebkitBackgroundClip: "text",
+                                    WebkitTextFillColor: "transparent",
+                                }}
+                            >
+                                {t("title1")}
+                            </Typography>
+                            <Title1 sx={{ fontWeight: 400 }}>
+                                {t("title2")}
+                            </Title1>
+                        </Box>
+                        <Subtitle1 sx={{ fontWeight: 500, textAlign: "center" }}>
+                            {t("titleGreet")}
+                        </Subtitle1>
                     </Stack>
                 </motion.div>
             </Box>
 
-        </Box>
+            <Stack sx={{ alignItems: "center", width: "100%" }}>
+                <motion.div
+                    style={{ width: "50rem" }}
+                    initial={{ opacity: 0, scale: 0 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, ease: "easeIn" }}
+                >
+                    
+                    <ShadowedStack sx={{ width: "50rem", height: "20rem", borderRadius: "2rem", background: "#FFF", p: "2rem" }}>
+                        <Subtitle1 sx={{ display: "inline" }}>
+                            {t.rich("greet", {
+                                emp: (children) => 
+                                    <Subtitle1
+                                        sx={{
+                                            display: "inline",
+                                            background: "-webkit-linear-gradient(15deg, #1E90FF, #7B68EE)",
+                                            WebkitBackgroundClip: "text",
+                                            WebkitTextFillColor: "transparent",
+                                        }}
+                                    >
+                                        {children}
+                                    </Subtitle1>
+                            })}
+                        </Subtitle1>
+                    </ShadowedStack>
+                </motion.div>
+            </Stack>
+        </Stack>
     )
 }
 
