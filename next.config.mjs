8a470412ca1f9a05a5510cb8c65282ddf00af5b6
CurlyRhapsody <matthewchan60792@gmail.com>
2026-04-29 23:34:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
   reactCompiler: true,
