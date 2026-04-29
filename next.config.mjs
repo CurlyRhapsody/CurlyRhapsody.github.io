e3dc34e9ee9e7ed75d911660f7404a0252fce6b3
CurlyRhapsody <matthewchan60792@gmail.com>
2026-04-29 23:37:17 +0800

    comment out reactCompiler

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  reactCompiler: true,
+  // reactCompiler: true,
 };
 
 export default withNextIntl(nextConfig);
