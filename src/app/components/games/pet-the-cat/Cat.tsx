470fa352158c42d0ddcf09cdb49ef5965ed49411
CurlyRhapsody <matthewchan60792@gmail.com>
2026-05-24 00:39:02 +0800

    Pet cat game fix - Forgot to save a file

--- src/app/components/games/pet-the-cat/Cat.tsx
+++ src/app/components/games/pet-the-cat/Cat.tsx
@@ -1,10 +1,13 @@
+import { Box } from "@mui/material";
 import { CatState } from "./PetCatGameBoard";
 
 const Cat = ({ gameState, onPet }: {
     gameState: CatState;
     onPet: (event: React.MouseEvent<HTMLDivElement>) => void;
 }) => {
-
+    return (
+        <Box></Box>
+    )
 }
 
 export default Cat;
